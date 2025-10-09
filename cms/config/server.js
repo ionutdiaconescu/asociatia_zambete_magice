c6b6a05dcade098884e56015bee9730ed4ecd5cd
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-10-09 20:52:54 +0300

    -

--- cms/config/server.js
+++ cms/config/server.js
@@ -2,6 +2,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://asociatia-zambete-magice.onrender.com"),
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
   },
