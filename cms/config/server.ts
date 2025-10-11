b63a50de42c64d4cd8d6b7c4f4c1df70eafc0bc6
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-10-11 14:22:59 +0300

    admin, databse, server.ts deleted

--- cms/config/server.ts
+++
@@ -1,9 +0,0 @@
-// server.ts – TypeScript version so Strapi loader treats it uniformly with other TS configs
-export default ({ env }) => {
-  const keys = env.array("APP_KEYS");
-  return {
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 1337),
-    app: { keys },
-  };
-};
