da0e850aa1f8c6471fc3ba9091f1a257ff76674f
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-10-14 20:42:53 +0300

    -supabase test

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,7 +1,12 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: env("DATABASE_CLIENT", "postgres"),
-    connection: env("DATABASE_URL"),
-    ssl: env.bool("DATABASE_SSL", true),
-  },
-});
+module.exports = ({ env }) => {
+  console.log("DATABASE_URL:", env("DATABASE_URL"));
+  console.log("DATABASE_CLIENT:", env("DATABASE_CLIENT"));
+  console.log("DATABASE_SSL:", env("DATABASE_SSL"));
+  return {
+    connection: {
+      client: env("DATABASE_CLIENT", "postgres"),
+      connection: env("DATABASE_URL"),
+      ssl: env.bool("DATABASE_SSL", true),
+    },
+  };
+};
