6ad20b0be39a99449b6b859a6aedb2c829397761
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-10-28 12:33:53 +0200

    -

--- cms/scripts/start-strapi-wrapper.js
+++ cms/scripts/start-strapi-wrapper.js
@@ -2,6 +2,10 @@
 "use strict";
 // Minimal production-only wrapper for Strapi startup (in-process)
 const { resolve } = require("path");
+console.log(
+  "[strapi-wrapper] Executing start-strapi-wrapper.js at",
+  new Date().toISOString()
+);
 const strapi = require("@strapi/strapi");
 
 strapi.createStrapi({ distDir: resolve(__dirname, "./dist") }).start();
