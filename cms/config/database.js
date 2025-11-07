de67b4b53ba2d56a9e52bc00c6ece50baafcb2fe
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-11-07 21:13:25 +0200

    Fix database timeout - optimize connection pool for Supabase

--- cms/config/database.js
+++ cms/config/database.js
@@ -7,6 +7,19 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
+    pool: {
+      min: 0,
+      max: 1,
+      acquireTimeoutMillis: 60000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+    },
+    debug: false,
   },
 });
