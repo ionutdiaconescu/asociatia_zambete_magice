d59357fb5caedbf6031c96f1a8980889f4b9ef3f
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-11-16 22:23:21 +0200

    fix: explicitly configure routes for homepage and campaigns

--- cms/src/api/campanie-de-donatii/routes/campanie-de-donatii.ts
+++ cms/src/api/campanie-de-donatii/routes/campanie-de-donatii.ts
@@ -2,6 +2,20 @@
  * campanie-de-donatii router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::campanie-de-donatii.campanie-de-donatii');
+export default factories.createCoreRouter(
+  "api::campanie-de-donatii.campanie-de-donatii",
+  {
+    config: {
+      find: {
+        policies: [],
+        middlewares: [],
+      },
+      findOne: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  }
+);
--- cms/src/api/homepage/routes/homepage.ts
+++ cms/src/api/homepage/routes/homepage.ts
@@ -2,6 +2,17 @@
  * homepage router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::homepage.homepage');
+export default factories.createCoreRouter("api::homepage.homepage", {
+  config: {
+    find: {
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      policies: [],
+      middlewares: [],
+    },
+  },
+});
