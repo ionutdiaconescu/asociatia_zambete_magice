769ad57021a9cae03e962d4c6b9657fae1a28f67
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2025-11-16 22:30:47 +0200

    fix: add missing index.ts files for API modules registration

---
+++ cms/src/api/campanie-de-donatii/index.ts
@@ -0,0 +1,9 @@
+import controller from "./controllers/campanie-de-donatii";
+import service from "./services/campanie-de-donatii";
+import routes from "./routes/campanie-de-donatii";
+
+export default {
+  controller,
+  service,
+  routes,
+};
---
+++ cms/src/api/homepage/index.ts
@@ -0,0 +1,9 @@
+import controller from "./controllers/homepage";
+import service from "./services/homepage";
+import routes from "./routes/homepage";
+
+export default {
+  controller,
+  service,
+  routes,
+};
